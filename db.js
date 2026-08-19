1029d2a01f824711302f4efb4af05a3d645e287d
muhammad dario varsel prayuda <muhammaddariovarselprayuda@MacBook-Air-muhammad.local>
2026-08-19 19:34:37 +0700

    Support Vercel serverless deployment

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const Database = require('better-sqlite3');
 
-const DB_PATH = process.env.DB_PATH || path.join(__dirname, 'data.sqlite');
+const DB_PATH = process.env.DB_PATH || (process.env.VERCEL ? path.join('/tmp', 'data.sqlite') : path.join(__dirname, 'data.sqlite'));
 const isNew = !fs.existsSync(DB_PATH);
 const db = new Database(DB_PATH);
 db.pragma('journal_mode = WAL');
